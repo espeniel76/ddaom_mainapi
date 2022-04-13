3808fb7ae18d5c49bb345f02e64e39a42d2aa3bd
espeniel76 <espeniel69@gmail.com>
2022-04-13 22:17:38 +0900

    .

--- db/따옴쿼리.sql
+++ db/따옴쿼리.sql
@@ -6,13 +6,3 @@ SELECT * FROM novel_step1 ns;
 SELECT * FROM novel_step2 ns;
 SELECT * FROM novel_step3 ns;
 SELECT * FROM novel_step4 ns;
-
-(SELECT seq_novel_step2, 0 AS seq_novel_step3 FROM novel_step2 WHERE seq_novel_step1 = 5 ORDER BY cnt_like DESC LIMIT 1)
-UNION
-(SELECT 0 AS seq_novel_step2, seq_novel_step3 FROM novel_step3 WHERE seq_novel_step1 = 5 ORDER BY cnt_like DESC LIMIT 1);
-
-(SELECT seq_novel_step2, 0 AS seq_novel_step3, 0 AS seq_novel_step4 FROM novel_step2 WHERE seq_novel_step1 = 1 ORDER BY cnt_like DESC LIMIT 1)
-UNION ALL
-(SELECT 0 AS seq_novel_step2, seq_novel_step3, 0 AS seq_novel_step4 FROM novel_step3 WHERE seq_novel_step1 = 1 ORDER BY cnt_like DESC LIMIT 1)
-UNION ALL
-(SELECT 0 AS seq_novel_step2, 0 AS seq_novel_step3, seq_novel_step4 FROM novel_step4 WHERE seq_novel_step1 = 1 ORDER BY cnt_like DESC LIMIT 1);
