8281f0cadd79ad7bf5b0d4ef75aab4b4317e041e
espeniel76 <espeniel69@gmail.com>
2022-04-18 20:29:35 +0900

    .

--- db/initial/따옴쿼리.sql
+++ db/initial/따옴쿼리.sql
@@ -1,27 +1,8 @@
-TRUNCATE table ddaom_user1.member_subscribes; 
+TRUNCATE table ddaom_user1.member_subscribes;
 TRUNCATE table ddaom_user2.member_subscribes;
-
-SELECT A.seq_member, A.seq_member_following
-FROM
-(
-        (SELECT seq_member, seq_member_following, created_at FROM ddaom_user1.member_subscribes WHERE subscribe_yn = true AND (seq_member = 8 OR seq_member_following = 8))
-        UNION ALL
-        (SELECT seq_member, seq_member_following, created_at FROM ddaom_user2.member_subscribes WHERE subscribe_yn = true AND (seq_member = 8 OR seq_member_following = 8))
-) AS A;
-
-SELECT A.seq_member, A.seq_member_following, A.created_at
-FROM
-(
-        (SELECT seq_member, seq_member_following, created_at FROM ddaom_user1.member_subscribes WHERE subscribe_yn = true AND (seq_member = 8 OR seq_member_following = 8))
-        UNION ALL
-        (SELECT seq_member, seq_member_following, created_at FROM ddaom_user2.member_subscribes WHERE subscribe_yn = true AND (seq_member = 8 OR seq_member_following = 8))
-) AS A
-ORDER BY A.created_at DESC
-LIMIT 0, 10;
+UPDATE ddaom.member_details SET cnt_subscribe = 0;
 
 SELECT * FROM ddaom_user1.member_subscribes;
 SELECT * FROM ddaom_user2.member_subscribes;
 
-(SELECT seq_member, seq_member_following, created_at FROM ddaom_user1.member_subscribes WHERE subscribe_yn = true AND (seq_member = 8 OR seq_member_following = 8))
-UNION ALL
-(SELECT seq_member, seq_member_following, created_at FROM ddaom_user2.member_subscribes WHERE subscribe_yn = true AND (seq_member = 8 OR seq_member_following = 8));
+SELECT seq_member, cnt_subscribe FROM ddaom.member_details;
