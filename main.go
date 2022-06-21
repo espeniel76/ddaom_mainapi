35865beb112eef8ae8739c92a6b9e5a394f0f420
espeniel76 <espeniel69@gmail.com>
2022-06-21 23:56:03 +0900

    .

--- main.go
+++ main.go
@@ -35,18 +35,18 @@ func setInitialize() {
 	})
 	handler := c.Handler(mux)
 
-	// // for REST HTTP
-	// err := http.ListenAndServe(":"+define.HTTP_PORT, handler)
-	// if err != nil {
-	// 	log.Fatal("ListenAndServe:", err)
-	// }
+	// for REST HTTP
+	err := http.ListenAndServe(":"+define.HTTP_PORT, handler)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 
 	// for REST SSL
 	// go func() {
-	err := http.ListenAndServeTLS(":"+define.HTTP_PORT_SSL, "/usr/local/ssl/cert.pem", "/usr/local/ssl/key.pem", handler)
-	if err != nil {
-		log.Fatal("ListenAndServeTLS:", err)
-	}
+	// err := http.ListenAndServeTLS(":"+define.HTTP_PORT_SSL, "/usr/local/ssl/cert.pem", "/usr/local/ssl/key.pem", handler)
+	// if err != nil {
+	// 	log.Fatal("ListenAndServeTLS:", err)
+	// }
 	// }()
 
 	fmt.Println("Test Edit")
