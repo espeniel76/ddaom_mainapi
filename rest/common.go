615e624a08a557ff6259b6d8a73657cd6b021c1f
espeniel76 <espeniel69@gmail.com>
2022-06-25 00:18:30 +0900

    .

--- rest/common.go
+++ rest/common.go
@@ -136,13 +136,13 @@ func common(f func(*domain.CommonRequest) domain.CommonResponse) func(w http.Res
 		intervalEnd := time.Now().UnixMilli()
 		fmt.Println(intervalStart, intervalEnd)
 
-		ret, err := fmt.Println(req.HttpRquest.Method)
-		fmt.Println(ret, err)
-		if _, err := fmt.Println(req.HttpRquest.Method); err != nil {
-			fmt.Println("이건 헬스체크")
-		} else {
-			fmt.Println("사용자 요청")
-		}
+		fmt.Println(r)
+		// fmt.Println(ret, err)
+		// if _, err := fmt.Println(req.HttpRquest.Method); err != nil {
+		// 	fmt.Println("이건 헬스체크")
+		// } else {
+		// 	fmt.Println("사용자 요청")
+		// }
 
 		// accessLog(&req, &res, intervalEnd, intervalStart)
 
