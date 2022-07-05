21a4bde2e8658551d1a84201359f17324265bf51
espeniel76 <espeniel69@gmail.com>
2022-07-05 14:38:04 +0900

    .

--- rest/handlers/common-db.go
+++ rest/handlers/common-db.go
@@ -407,7 +407,7 @@ func educeImage(seqColor int64, seqImage int64, seqNovelStep1 int64) {
 			return
 		}
 
-		fmt.Println(strings.Replace(imgSrc, "/upload", "upload", 1))
+		fmt.Println(strings.Replace(imgSrc, "/tmp/", "", 1))
 
 		s3.DownloadFile("/tmp/thumb", strings.Replace(imgSrc, "/upload", "upload", 1))
 		return
